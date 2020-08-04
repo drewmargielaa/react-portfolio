8d79ea0c9a4f7b966c85eb0524475edf742188cd
Andrew Souza <drewmargielaa@gmail.com>
2020-08-04 18:30:39 -0400

    Updates

---
+++ precache-manifest.102dac5803a2940ee0f33f5ceb96077d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6074725b7b8a0be8e09827e9df2b55f4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1c36b75d3d1e9071a810",
+    "url": "/react-portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "7fbe71f88d0da0f2cf7a",
+    "url": "/react-portfolio/static/css/main.1daa45c7.chunk.css"
+  },
+  {
+    "revision": "1c36b75d3d1e9071a810",
+    "url": "/react-portfolio/static/js/2.e3f9d426.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react-portfolio/static/js/2.e3f9d426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fbe71f88d0da0f2cf7a",
+    "url": "/react-portfolio/static/js/main.f4ff1628.chunk.js"
+  },
+  {
+    "revision": "154b7a8cbf9aee46af3e",
+    "url": "/react-portfolio/static/js/runtime-main.4cf6e595.js"
+  }
+]);
